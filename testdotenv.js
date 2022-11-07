0186c65f9dcc34d1b37eb8a3fb1e1e88b633fafb
Jonathan Garonian <j.garonian@gmail.com>
2022-11-07 09:45:21 +0100

    commit test

---
+++ testdotenv.js
@@ -0,0 +1,5 @@
+const userName = process.env.userName;
+const userCity = process.env.userCity;
+const userLanguage = process.env.userLanguage;
+
+console.log(`I am ${userName}, wilder in ${userCity} and I love ${userLanguage}!`);
