da241d2706b284a4a9d4dc61949d332adb48a51b
Jonathan Garonian <j.garonian@gmail.com>
2022-11-07 09:48:09 +0100

    Revert "commit test"
    This reverts commit 0186c65f9dcc34d1b37eb8a3fb1e1e88b633fafb.

--- testdotenv.js
+++
@@ -1,5 +0,0 @@
-const userName = process.env.userName;
-const userCity = process.env.userCity;
-const userLanguage = process.env.userLanguage;
-
-console.log(`I am ${userName}, wilder in ${userCity} and I love ${userLanguage}!`);
